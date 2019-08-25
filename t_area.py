15d4237d6f0eb4d27137b7e3e362aa9c3c50a2cd
CH08 <44739825+CH08@users.noreply.github.com>
2019-08-25 14:15:49 -0400

    Add files via upload
    Simple Python code to find the area of a triangle.

---
+++ t_area.py
@@ -0,0 +1,17 @@
+#In this program, we will be finding the area of a triangle. In order to find
+#the area, we must use the formula area=1/2*base*height.
+
+#First, we will assign a value to our variables Base and Height. We will keep
+#these variable names simple and descriptive by naming them "base" and "height"
+
+#assign the value 12 to the variable "base"
+base=12
+#assign the value 16 to the variable "height"
+height=16
+
+#Next we need to assign our formula to calculate the area. We will, again, keep
+#this name simple and descriptive by naming it "area".
+area=1/2*base*height
+#Now we will tell python to print the "area" which will calculate our formula
+#12*16 and then divide that in half, giving us our value. 
+print(area)
